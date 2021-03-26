1cbc4d1d53b16bb8c8bc1ff0f0dd12b0abde9ade
vicpack <childrenofpunk@hotmail.com>
2021-03-26 00:06:48 -0600

    rutas con app-routing-module

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,37 @@
 import { NgModule } from '@angular/core';
-@NgModule({
+import { RouterModule, Routes } from '@angular/router';
+import { PageLoginComponent } from './core/presentation/pages/page-login/page-login.component';
 
+const routes: Routes = [
+    { path: '', component: PageLoginComponent },
+    {
+        path: 'medics', loadChildren: () =>
+            import("./medics/medics.module").then(m => m.MedicsModule)
+    },
+    {
+        path: 'histories', loadChildren: () =>
+            import("./histories/histories.module").then(m => m.HistoriesModule)
+    },
+    {
+        path: 'users', loadChildren: () =>
+            import("./users/users.module").then(m => m.UsersModule)
+    },
+    {
+        path: 'drivers', loadChildren: () =>
+            import("./drivers/drivers.module").then(m => m.DriversModule)
+    },
+    {
+        path: 'dashboard', loadChildren: () =>
+            import("./dashboard/dashboard.module").then(m => m.DashboardModule)
+    },
+    {
+        path: '**', redirectTo: ''
+    },
+    // { path: '**', component: PageLoginComponent },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],//para recorrer las rutas
+    exports: [RouterModule]
 })
 
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,48 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import { CoreModule } from './core/core.module';
-import { RouterModule, Routes } from '@angular/router';
-import { PageLoginComponent } from './core/presentation/pages/page-login/page-login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing-module';
 
 
 
 //interfase para las rutas
-const routes: Routes = [
-  { path: '', component: PageLoginComponent },
-  {
-    path: 'medics', loadChildren: () =>
-      import("./medics/medics.module").then(m => m.MedicsModule)
-  },
-  {
-    path: 'histories', loadChildren: () =>
-      import("./histories/histories.module").then(m => m.HistoriesModule)
-  },
-  {
-    path: 'users', loadChildren: () =>
-      import("./users/users.module").then(m => m.UsersModule)
-  },
-  {
-    path: 'drivers', loadChildren: () =>
-      import("./drivers/drivers.module").then(m => m.DriversModule)
-  },
-  {
-    path: 'dashboard', loadChildren: () =>
-      import("./dashboard/dashboard.module").then(m => m.DashboardModule)
-  },
-  {
-    path: '**', redirectTo: ''
-  },
-  // { path: '**', component: PageLoginComponent },
-];
+/**/
 //importacion de modulos 
 @NgModule({
   declarations: [AppComponent, ItemComponent],
   imports: [
-    BrowserModule,
-    CoreModule,
-    RouterModule.forRoot(routes),
-    BrowserAnimationsModule,//metodo statico forRoot para las rutas
+    BrowserModule, CoreModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
