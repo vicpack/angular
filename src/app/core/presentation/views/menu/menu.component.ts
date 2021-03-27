ace6ceb8733523da89f6722ca947b083e17d4e70
vicpack <childrenofpunk@hotmail.com>
2021-03-26 23:11:02 -0600

    agregando menu con List

--- src/app/core/presentation/views/menu/menu.component.ts
+++ src/app/core/presentation/views/menu/menu.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IMenu {
+  title: string
+  url: string
+}
 @Component({
   selector: 'amb-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  listMenu: IMenu[] = [
+    { title: "Resumen", url: "/dashboard" },
+    { title: "Historias", url: "/histories" },
+    { title: "Médicos", url: "/medics" },
+    { title: "Pilotos", url: "/drivers" },
+    { title: "Usuarios", url: "/users" },
+  ];
   constructor() { }
 
   ngOnInit(): void {
