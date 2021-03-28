0203dbdfa22b1584aaebcf06905fee20433ad7d9
vicpack <childrenofpunk@hotmail.com>
2021-03-28 09:28:34 -0600

    clase3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TitleComponent } from './components/title/title.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [TitleComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, MatToolbarModule, MatIconModule,
+  ],
+  exports: [TitleComponent, MatToolbarModule, MatIconModule]
 })
 export class SharedModule { }
