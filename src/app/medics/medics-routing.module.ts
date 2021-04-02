ab15a93e10b0eec3d210c2f3f96741e1f9fba3ec
vicpack <childrenofpunk@hotmail.com>
2021-04-02 15:53:25 -0600

    agregando cards

--- src/app/medics/medics-routing.module.ts
+++ src/app/medics/medics-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListMedicsComponent } from './presentation/views/list-medics/list-medics.component';
+import { PageMedicComponent } from './presentation/pages/page-medic/page-medic.component';
 
 const routes: Routes = [
-  { path: "list", component: ListMedicsComponent }
+  { path: "", component: PageMedicComponent }
 ];
 
 @NgModule({
