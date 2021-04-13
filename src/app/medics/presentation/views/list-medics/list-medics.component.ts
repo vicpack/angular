789c2f33d3e7172029d670fd9350101b5648385a
vicpack <childrenofpunk@hotmail.com>
2021-04-12 22:04:55 -0500

    agregando eliminacion en medicos

--- src/app/medics/presentation/views/list-medics/list-medics.component.ts
+++ src/app/medics/presentation/views/list-medics/list-medics.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PaginatorData } from 'src/app/shared/classes/paginator-data';
 import { MetaDataColumn } from 'src/app/shared/services/meta-data-column';
+import { UtilsService } from 'src/app/shared/services/utils.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -19,28 +21,43 @@ export class ListMedicsComponent extends PaginatorData implements OnInit {
 
   ];
   data: any = [
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
-    { cmp: "123", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "1", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "2", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "3", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "4", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "5", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "6", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "7", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "8", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "9", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "10", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "11", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "12", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "13", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "14", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "15", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
+    { cmp: "16", nombre: "Gregory", apellido: "House", especialidad: "Cirujano", localidad: "Perú" },
   ];
 
-  constructor() {
+  constructor(private readonly utils: UtilsService) {
     super();
   }
 
+  delete(record: any) {
+    const observableConfirm: Observable<string> = this.utils.confirm(`¿Realmente quiere eliminar "${record.nombre} ${record.apellido}"?`);
+
+
+    observableConfirm.subscribe((response: string) => {
+      if (!response) {
+        return;
+      }
+      const matchedRecord = this.data.findIndex((el: any) => el.id === record.id);
+      this.data.splice(matchedRecord, 1);
+      this.loadData();
+
+    });
+
+  }
   ngOnInit(): void {
     this.loadData();
   }
