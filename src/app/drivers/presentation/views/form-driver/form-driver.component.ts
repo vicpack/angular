ba2f47068f76085b5a72b2665c67a8daee92cce5
vicpack <childrenofpunk@hotmail.com>
2021-04-21 22:31:56 -0500

    agregando que los campos sean obligatorios

--- src/app/drivers/presentation/views/form-driver/form-driver.component.ts
+++ src/app/drivers/presentation/views/form-driver/form-driver.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -8,8 +9,16 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   encapsulation: ViewEncapsulation.None,
 })
 export class FormDriverComponent implements OnInit {
-  title: string = ""
-  constructor(@Inject(MAT_DIALOG_DATA) private data: any) { }
+  title: string = "";
+  fg: FormGroup;
+
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any) {
+    this.fg = new FormGroup({
+      nombre: new FormControl(this.data?.nombre, Validators.required),
+      apellido: new FormControl(this.data?.apellido, Validators.required),
+      licencia: new FormControl(this.data?.licencia, Validators.required),
+    });
+  }
 
   ngOnInit(): void {
     this.title = this.data ? "Edición" : "Nuevo";
